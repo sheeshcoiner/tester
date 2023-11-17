00f1fa6c9be968184fb06d4d7aa8b4c06bd2ef95
sheeshcoiner <123221502+sheeshcoiner@users.noreply.github.com>
2023-11-16 19:17:37 -0800

    Add files via upload

---
+++ deeznuts.js
@@ -0,0 +1,6 @@
+{
+	"name": "Testikles",
+	"symbol": "TSKL",
+	"description": "deez nuts",
+	"image": "https://static.wikia.nocookie.net/siivagunner/images/2/20/Deeznuts.jpg/revision/latest?cb=20201208014948"
+}
